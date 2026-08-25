04efdba765b809e8d9a8c172f7ac0f9874fcb17c
Nicholas <Nickmilanin@gmail.com>
2026-08-24 23:50:39 -0300

    Consulta com vários critérios

--- src/main/java/com/nelioalves/workshopmongo/resources/util/URL.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/util/URL.java
@@ -2,6 +2,9 @@ package com.nelioalves.workshopmongo.resources.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
 
@@ -13,6 +16,19 @@ public class URL {
 		catch (UnsupportedEncodingException e) {
 			return "";
 		}
+	}
+	
+	public static Date convertDate (String textDate, Date defaultValue) {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		try {
+			return sdf.parse(textDate);
+		}
+		catch (ParseException e) {
+			return defaultValue;
+		}
+		
 	}
 	
 }
--- src/main/java/com/nelioalves/workshopmongo/tepository/PostRepository.java
+++ src/main/java/com/nelioalves/workshopmongo/tepository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.nelioalves.workshopmongo.tepository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -29,6 +30,11 @@ public interface PostRepository extends MongoRepository<Post, String> {
 	
 	List<Post> findByTitleContainingIgnoreCase (String text);
 	
-	@Query("{ 'Title': {$regex: ?0, $options: 'i'}}")
+	@Query("{ 'title': {$regex: ?0, $options: 'i'}}")
 	List<Post> findByTitle (String text);
+	
+	
+
+	@Query("{ $and: [ { date: {$$gte: ?1} }, { date: {$lte: ?2} }, {$or: [ { 'title': {$regex: ?0, $options: 'i'}}, { 'body': {$regex: ?0, $options: 'i'}}, { 'comments.text': {$regex: ?0, $options: 'i'}} ]} ]}")
+	List<Post> fullSearch (String text, Date minDate, Date maxDate);
 }
